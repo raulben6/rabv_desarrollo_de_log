fe2704887a84a9164407a4c236632dea36915498
Raul Antonio Benitez Vasquez <ia.bv100420@ufg.edu.sv>
2025-05-15 12:04:05 -0600

    cuarto push

--- core/process.php
+++ core/process.php
@@ -1,8 +1,8 @@
 <?php
-ini_set('display_error' , 1);
-ini_set('display_startup_error' , 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 include('inc/funciones.inc.php');
-include('secure/ips.php)');
+include('secure/ips.php');
 
 $metodo_permitido = "POST";
 $archivo = "../logs/log.log";
@@ -11,88 +11,56 @@ $ip = ip_in_ranges($_SERVER["REMOTE_ADDR"], $rango);
 $txt_usuario_autorizado = "admin";
 $txt_password_autorizado = "admin";
 
+// Verifica que exista HTTP_REFERER
+if(array_key_exists("HTTP_REFERER", $_SERVER)) {
+    if(strpos($_SERVER["HTTP_REFERER"], $dominio_autorizado) !== false) {
+        if($ip === true) {
+            if($_SERVER["REQUEST_METHOD"] === $metodo_permitido) {
 
-//SE VERIFICA QUE LA DIRECCION DE ORIGEN SEA AUTORIZADA
-if(array_key_exists("HTTP_REFERER",$_SERVER)){
-    //VIENE DE UNA PAGINA DENTRO DEL SISTEMA
+                $valor_campo_usuario = (array_key_exists("txt_user", $_POST)) ? htmlspecialchars(stripslashes(trim($_POST["txt_user"])), ENT_QUOTES) : "";
+                $valor_campo_password = (array_key_exists("txt_pass", $_POST)) ? htmlspecialchars(stripslashes(trim($_POST["txt_pass"])), ENT_QUOTES) : "";
 
-    if(strpos($_SERVER["HTTP_REFERER"],$dominio_autorizado)){
+                if($valor_campo_usuario != "" && $valor_campo_password != "") {
+                    $usuario = preg_match('/^[a-zA-Z0-9]{1,10}$/', $valor_campo_usuario);
+                    $password = preg_match('/^[a-zA-Z0-9]{1,10}$/', $valor_campo_password);
 
-        if($ip === true){
-
-            if($SERVER["REQUEST_METHOD"] == $metodo_permitido){
-                //LIMPIEZA DE VALORES DESDE QUE VIENEN DESDE EL FORMULARIO
-                $valor_campo_usuario = (     (array_key_exists("txt_user" ,$POST)) ? htmlspecialchars(stripslashes(trim($_POST["txt_user"])),ENT_QUOTES) :"" );
-                $valor_campo_password = (     (array_key_exists("txt_pass" ,$POST)) ? htmlspecialchars(stripslashes(trim($_POST["txt_pass"])),ENT_QUOTES) :"" );
-
-
-                //SE VERIFICA QUE LOS VALORES DE LOS CAMPOS SEA DIFERENTE A VACIO
-
-                if(($valor_campo_usuario != "" || strlen($valor_campo_usuario) > 0) and 
-                ($valor_campo_password != "" || strlen($valor_campo_password) > 0) ){
-
-                    $usuario = preg_match('/^[a-zA-Z0-9]{1,10}+$/' , $valor_campo_usuario); //SE VERIFICA CON UN PATRON SI EL VALOR DEL CAMPO USUARIO CUMPLE CON LAS CONDICIONES ACEPTABLES 
-                    $password = preg_match('/^[a-zA-Z0-9]{1,10}+$/' , $valor_campo_password); //SE VERIFICA CON UN PATRON SI EL VALOR DEL CAMPO USUARIO CUMPLE CON LAS CONDICIONES ACEPTABLES 
-
-                    if($usuario !== false and $usuario !== 0 and $password !== false and $password !==0){
-
-                        if($valor_campo_usuario === $txt_usuario_autorizado and $valor_campo_password === $txt_password_autorizado){
-
+                    if($usuario === 1 && $password === 1) {
+                        if($valor_campo_usuario === $txt_usuario_autorizado && $valor_campo_password === $txt_password_autorizado) {
                             echo("HOLA MUNDO");
-                            crear_editar_log($archivo , "EL CLIENTE INICIO SESION SATISFACTORIAMENTE" , 1,$SERVER["REMOTE_ADDR"],$SERVER["HTTP_REFERER"],$SERVER["HTTP_USER_AGENT"]);
-
+                            crear_editar_log($archivo, "EL CLIENTE INICIO SESION SATISFACTORIAMENTE", 1, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+                        } else {
+                            crear_editar_log($archivo, "CREDENCIALES INCORRECTAS ENVIADAS HACIA // ".$_SERVER["HTTP_HOST"]." ".$_SERVER["REQUEST_URI"], 2, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+                            header ("HTTP/1.1 301 Moved Permanently");
+                            header("Location: ../?status=7");
                         }
-                        else{
-                            crear_editar_log($archivo , "CREDENCIALES INCORECTAS ENVIADAS HACIA // $SERVER[HTTP_HOST] $SERVER[HTTP_REQUEST_URI]", 2,$SERVER["REMOTE_ADDR"],$SERVER["HTTP_REFERER"],$SERVER["HTTP_USER_AGENT"]);
-                            header ("HTTP/1.1 301 Moved Permanetly");
-                            header("Location: ..//?status = 6");
-                        }
-
-
-                    }else{
-
-                        crear_editar_log($archivo , "ENVIO DE DATOS DE FORMULARIO CON DATOS NO ACEPTADOS" , 3,$SERVER["REMOTE_ADDR"],$SERVER["HTTP_REFERER"],$SERVER["HTTP_USER_AGENT"]);
-                        header ("HTTP/1.1 301 Moved Permanetly");
-                        header("Location: ..//?status = 6");
-
-
+                    } else {
+                        crear_editar_log($archivo, "ENVIO DE DATOS DE FORMULARIO CON DATOS NO ACEPTADOS", 3, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+                        header ("HTTP/1.1 301 Moved Permanently");
+                        header("Location: ../?status=6");
                     }
-
-
-
+                } else {
+                    crear_editar_log($archivo, "ENVIO DE CAMPOS VACIOS AL SERVIDOR", 3, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+                    header ("HTTP/1.1 301 Moved Permanently");
+                    header("Location: ../?status=5");
                 }
-            }else {
-
-                crear_editar_log($archivo , "ENVIO DE CAMPOS VACIOS AL SERVIDOR" , 3,$SERVER["REMOTE_ADDR"],$SERVER["HTTP_REFERER"],$SERVER["HTTP_USER_AGENT"]);
-                header ("HTTP/1.1 301 Moved Permanetly");
-                header("Location: ..//?status = 5");
-
-            }            
-        }else{
-
-            crear_editar_log($archivo , "ENVIO DE METODO NO AUTORIZADO" , 3,$SERVER["REMOTE_ADDR"],$SERVER["HTTP_REFERER"],$SERVER["HTTP_USER_AGENT"]);
-            header ("HTTP/1.1 301 Moved Permanetly");
-            header("Location: ..//?status = 4");
-
-
-
+            } else {
+                crear_editar_log($archivo, "ENVIO DE METODO NO AUTORIZADO", 3, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+                header ("HTTP/1.1 301 Moved Permanently");
+                header("Location: ../?status=4");
+            }
+        } else {
+            crear_editar_log($archivo, "LA DIRECCION IP NO ESTA AUTORIZADA", 3, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+            header ("HTTP/1.1 301 Moved Permanently");
+            header("Location: ../?status=3");
         }
     } else {
-
-        crear_editar_log($archivo , "LA DIRECCION IP NO ESTA AUTORIZADA" , 3,$SERVER["REMOTE_ADDR"],$SERVER["HTTP_REFERER"],$SERVER["HTTP_USER_AGENT"]);
-        header ("HTTP/1.1 301 Moved Permanetly");
-        header("Location: ..//?status = 3");
-
-        
-
-
+        crear_editar_log($archivo, "DOMINIO NO AUTORIZADO", 3, $_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"], $_SERVER["HTTP_USER_AGENT"]);
+        header ("HTTP/1.1 301 Moved Permanently");
+        header("Location: ../?status=2");
     }
-
-
-}else {
-
-
+} else {
+    crear_editar_log($archivo, "NO SE DETECTA REFERER", 3, $_SERVER["REMOTE_ADDR"], "NO_REFERER", $_SERVER["HTTP_USER_AGENT"]);
+    header ("HTTP/1.1 301 Moved Permanently");
+    header("Location: ../?status=1");
 }
-
-
 ?>
